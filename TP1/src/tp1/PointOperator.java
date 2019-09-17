adf0b134dae9b39b61674e26ec22798300cb0f61
nabil-dbz <nbl.dabouz@gmail.com>
2019-09-17 11:51:39 -0400

    Ajout du TP1

---
+++ TP1/src/tp1/PointOperator.java
@@ -0,0 +1,40 @@
+package tp1;
+
+import java.util.*;
+
+public final class PointOperator {
+    // TODO appliquer la translation sur le vecteur d'entree.
+    public static Double[] translate(Double[] vector, Double[] translateVector) {
+        return null;
+    }
+
+    // TODO appliquer la rotation sur le vecteur d'entree.
+    public static Double[] rotate(Double[] vector, Double[][] rotationMatrix) {
+        return null;
+    }
+
+    // TODO appliquer le facteur de division sur le vecteur d'entree.
+    public static Double[] divide(Double[] vector, Double divider) {
+        return null;
+    }
+
+    // TODO appliquer le facteur de multiplication sur le vecteur d'entree.
+    public static Double[] multiply(Double[] vector, Double multiplier) {
+        return null;
+    }
+
+    // TODO appliquer le facteur d'addition sur le vecteur d'entree.
+    public static Double[] add(Double[] vector, Double adder) {
+        return null;
+    }
+
+    // TODO retourne la coordonnee avec les plus grandes valeurs en X et en Y.
+    public static Point2d getMaxCoord(Collection<Point2d> coords) {
+        return null;
+    }
+
+    // TODO retourne la coordonnee avec les plus petites valeurs en X et en Y.
+    public static Point2d getMinCoord(Collection<Point2d> coords) {
+        return null;
+    }
+}
